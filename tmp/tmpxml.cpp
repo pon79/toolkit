3867188a616f14de0816eba95446aaa40e4343ee
pon79 <pon79job@gmail.com>
2020-03-10 11:24:40 +0300

    корректировка GetCapabilitiesResponse

--- tmp/tmpxml.cpp
+++ tmp/tmpxml.cpp
@@ -41,57 +41,57 @@ void TmpXML::createGetCapabilitiesResponse()
     m_xmlWriter.writeStartElement( m_tds, "GetCapabilitiesResponse" );
     m_xmlWriter.writeStartElement( m_tds, "Capabilities" );
 
-    m_xmlWriter.writeStartElement( m_tt, "Analytics" );
+    m_xmlWriter.writeStartElement( m_tds, "Analytics" );
     m_xmlWriter.writeTextElement(m_tt, "XAddr", "http://" + m_hostAddress.toString() + "/onvif/analytics_service");
     m_xmlWriter.writeTextElement(m_tt, "RuleSupport", "True");
     m_xmlWriter.writeTextElement(m_tt, "AnalyticsModuleSupport", "True");
     m_xmlWriter.writeEndElement(); // close Analytics
 
-    m_xmlWriter.writeStartElement( m_tt, "Device" );
+    m_xmlWriter.writeStartElement( m_tds, "Device" );
 
     m_xmlWriter.writeTextElement(m_tt, "XAddr", "http://" + m_hostAddress.toString() + "/onvif/device_service");
-    m_xmlWriter.writeStartElement( m_tt, "Network" );
+    m_xmlWriter.writeStartElement( m_tds, "Network" );
     m_xmlWriter.writeTextElement(m_tt, "IPFilter", "false");
     m_xmlWriter.writeTextElement(m_tt, "ZeroConfiguration", "false");
     m_xmlWriter.writeTextElement(m_tt, "IPVersion6", "false");
     m_xmlWriter.writeTextElement(m_tt, "DynDNS", "false");
     m_xmlWriter.writeEndElement(); // close Network
 
-    m_xmlWriter.writeStartElement( m_tt, "System" );
+    m_xmlWriter.writeStartElement( m_tds, "System" );
     m_xmlWriter.writeTextElement(m_tt, "DiscoveryResolve", "false");
     m_xmlWriter.writeTextElement(m_tt, "DiscoveryBye", "false");
     m_xmlWriter.writeTextElement(m_tt, "RemoteDiscovery", "false");
     m_xmlWriter.writeTextElement(m_tt, "SystemBackup", "false");
     m_xmlWriter.writeTextElement(m_tt, "SystemLogging", "false");
     m_xmlWriter.writeTextElement(m_tt, "FirmwareUpgrade", "false");
-    m_xmlWriter.writeStartElement( m_tt, "SupportedVersions" );
+    m_xmlWriter.writeStartElement( m_tds, "SupportedVersions" );
     m_xmlWriter.writeTextElement(m_tt, "Major", "19");
     m_xmlWriter.writeTextElement(m_tt, "Minor", "12");
     m_xmlWriter.writeEndElement(); // close SupportedVersions
-    m_xmlWriter.writeStartElement( m_tt, "SupportedVersions" );
+    m_xmlWriter.writeStartElement( m_tds, "SupportedVersions" );
     m_xmlWriter.writeTextElement(m_tt, "Major", "2");
     m_xmlWriter.writeTextElement(m_tt, "Minor", "40");
     m_xmlWriter.writeEndElement(); // close SupportedVersions
-    m_xmlWriter.writeStartElement( m_tt, "SupportedVersions" );
+    m_xmlWriter.writeStartElement( m_tds, "SupportedVersions" );
     m_xmlWriter.writeTextElement(m_tt, "Major", "2");
     m_xmlWriter.writeTextElement(m_tt, "Minor", "20");
     m_xmlWriter.writeEndElement(); // close SupportedVersions
-    m_xmlWriter.writeStartElement( m_tt, "SupportedVersions" );
+    m_xmlWriter.writeStartElement( m_tds, "SupportedVersions" );
     m_xmlWriter.writeTextElement(m_tt, "Major", "2");
     m_xmlWriter.writeTextElement(m_tt, "Minor", "10");
     m_xmlWriter.writeEndElement(); // close SupportedVersions
-    m_xmlWriter.writeStartElement( m_tt, "SupportedVersions" );
+    m_xmlWriter.writeStartElement( m_tds, "SupportedVersions" );
     m_xmlWriter.writeTextElement(m_tt, "Major", "2");
     m_xmlWriter.writeTextElement(m_tt, "Minor", "0");
     m_xmlWriter.writeEndElement(); // close SupportedVersions
     m_xmlWriter.writeEndElement(); // close System
 
-    m_xmlWriter.writeStartElement( m_tt, "IO" );
+    m_xmlWriter.writeStartElement( m_tds, "IO" );
     m_xmlWriter.writeTextElement(m_tt, "InputConnectors", "1");
     m_xmlWriter.writeTextElement(m_tt, "RelayOutputs", "1");
     m_xmlWriter.writeEndElement(); // close IO
 
-    m_xmlWriter.writeStartElement( m_tt, "Security" );
+    m_xmlWriter.writeStartElement( m_tds, "Security" );
     m_xmlWriter.writeTextElement(m_tt, "TLS1.1", "false");
     m_xmlWriter.writeTextElement(m_tt, "TLS1.2", "false");
     m_xmlWriter.writeTextElement(m_tt, "OnboardKeyGeneration", "false");
@@ -104,36 +104,36 @@ void TmpXML::createGetCapabilitiesResponse()
 
     m_xmlWriter.writeEndElement(); // close Device
 
-    m_xmlWriter.writeStartElement( m_tt, "Events" );
+    m_xmlWriter.writeStartElement( m_tds, "Events" );
     m_xmlWriter.writeTextElement(m_tt, "XAddr", "http://" + m_hostAddress.toString() + "/onvif/event_service");
     m_xmlWriter.writeTextElement(m_tt, "WSSubscriptionPolicySupport", "false");
     m_xmlWriter.writeTextElement(m_tt, "WSPullPointSupport", "false");
     m_xmlWriter.writeTextElement(m_tt, "WSPausableSubscriptionManagerInterfaceSupport", "false");
     m_xmlWriter.writeEndElement(); // close Events
 
-    m_xmlWriter.writeStartElement( m_tt, "Imaging" );
+    m_xmlWriter.writeStartElement( m_tds, "Imaging" );
     m_xmlWriter.writeTextElement(m_tt, "XAddr", "http://" + m_hostAddress.toString() + "/onvif/image_service");
     m_xmlWriter.writeEndElement(); // close Imaging
 
-    m_xmlWriter.writeStartElement( m_tt, "Media" );
+    m_xmlWriter.writeStartElement( m_tds, "Media" );
     m_xmlWriter.writeTextElement(m_tt, "XAddr", "http://" + m_hostAddress.toString() + "/onvif/media_service");
-    m_xmlWriter.writeStartElement( m_tt, "StreamingCapabilities" );
+    m_xmlWriter.writeStartElement( m_tds, "StreamingCapabilities" );
     m_xmlWriter.writeTextElement(m_tt, "RTPMulticast", "false");
     m_xmlWriter.writeTextElement(m_tt, "RTP_TCP", "false");
     m_xmlWriter.writeTextElement(m_tt, "RTP_RTSP_TCP", "false");
     m_xmlWriter.writeEndElement(); // close StreamingCapabilities
     m_xmlWriter.writeEndElement(); // close Media
 
-    m_xmlWriter.writeStartElement( m_tt, "Media" );
+    m_xmlWriter.writeStartElement( m_tds, "Media" );
     m_xmlWriter.writeTextElement(m_tt, "XAddr", "http://" + m_hostAddress.toString() + "/onvif/media_service");
-    m_xmlWriter.writeStartElement( m_tt, "StreamingCapabilities" );
+    m_xmlWriter.writeStartElement( m_tds, "StreamingCapabilities" );
     m_xmlWriter.writeTextElement(m_tt, "RTPMulticast", "false");
     m_xmlWriter.writeTextElement(m_tt, "RTP_TCP", "false");
     m_xmlWriter.writeTextElement(m_tt, "RTP_RTSP_TCP", "false");
     m_xmlWriter.writeEndElement(); // close StreamingCapabilities
     m_xmlWriter.writeEndElement(); // close Media
 
-    m_xmlWriter.writeStartElement( m_tt, "PTZ" );
+    m_xmlWriter.writeStartElement( m_tds, "PTZ" );
     m_xmlWriter.writeTextElement(m_tt, "XAddr", "http://" + m_hostAddress.toString() + "/onvif/ptz_service");
     m_xmlWriter.writeEndElement(); // close PTZ
 
